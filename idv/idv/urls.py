676a253d4e377c50b58c9d3b5b8b7f6db985e18e
Stephan Huiser <stephan@huiser.nl>
2014-01-10 13:05:16 +0100

    Clean urls.py, remove admin crap to fix ImproperlyConfigured exceptions :(

--- idv/idv/urls.py
+++ idv/idv/urls.py
@@ -2,14 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    #url(r'^$', 'idv.views.home', name='home'),
-    url(r'^/?$', TemplateView.as_view(template_name="frontpage.html")),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name='frontpage.html'), name='home'),
+    url(r'^producten$', TemplateView.as_view(template_name="producten.html"), name="producten"),
     url(r'^admin/', include(admin.site.urls)),
+    # url(r'^blog/', include('blog.urls')),
 )
-
-
