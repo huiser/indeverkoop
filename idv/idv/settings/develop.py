cbbab108f2ff5376a0411cdbc2ceca5e2aa77194
Stephan Huiser <stephan@huiser.nl>
2014-01-10 12:59:24 +0100

    Change develop settings

--- idv/idv/settings/develop.py
+++ idv/idv/settings/develop.py
@@ -21,12 +21,13 @@ DATABASES = {
 	}
 }
 
-INSTALLED_APPS += (
-    "debug_toolbar",
-)
+if DEBUG:
+    INSTALLED_APPS += (
+        "debug_toolbar",
+    )
+    MIDDLEWARE_CLASSES += (
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    )
 
 INTERNAL_IPS = ("127.0.0.1",)
 
-MIDDLEWARE_CLASSES += (
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-)
