545fd409f957389094ef7230cff0e40f2858b332
Stephan Huiser <stephan@huiser.nl>
2014-01-30 09:36:59 +0100

    Fab fix

--- idv/idv/settings/production.py
+++ idv/idv/settings/production.py
@@ -3,8 +3,8 @@ from .base import *
 DATABASES = {
 	"default": {
 		"ENGINE":	"django.db.backends.postgresql_psycopg2",
-		"NAME":		"idv_production",
-		"USER":		"idv_production",
+		"NAME":		"idv_prod",
+		"USER":		"idv_prod",
 		"PASSWORD":	get_env_variable("DB_PASSWORD"),
 		"HOST":		"localhost",
 		"PORT":		"",
